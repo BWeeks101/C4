08c324fc8ca63e7f2a9e8d4430ded8d1437e2b92
BWeeks101 <bweeks101@gmail.com>
2020-11-14 13:16:00 +0000

    Added dynamic sidenav size calculation

--- assets/js/sidenav.js
+++ assets/js/sidenav.js
@@ -3,19 +3,56 @@
 * https://www.w3schools.com/howto/howto_js_sidenav.asp
 */
 
+onLoad();
+
+function onLoad() {
+    closeNav();    
+}
+
+/* Return width and negative offset for sideNav */
+function getNavWidth() {    
+    let navWidth = calcNavWidth();
+    
+    if (navWidth < 310) {
+        navWidth = 310;
+    }
+
+    let navOffset = (0 - navWidth);
+    
+    let navArray = [navWidth + "px", navOffset + "px"];
+    
+    return navArray;
+}
+
+/* Calculate width of sideNav */
+function calcNavWidth() {
+    let sWidth = screen.width;
+    let percentage = 33;
+
+    return (sWidth/100)*percentage;
+}
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("sideNav").style.left = "0px";
-  document.getElementById("sideNavDocOverlay").style.zIndex = "999";
-  document.getElementById("sideNavDocOverlay").style.backgroundColor = "rgba(0,0,0,0.7)";
-  document.getElementById("sideNavDocOverlay").style.left = "250px"
-
+    let navArray = getNavWidth();
+    let navWidth = navArray[0];
+    let navOffset = navArray[1];
+        
+    document.getElementById("sideNav").style.left = "0px";
+    document.getElementById("sideNavDocOverlay").style.zIndex = "999";
+    document.getElementById("sideNavDocOverlay").style.backgroundColor = "rgba(0,0,0,0.7)";
+    document.getElementById("sideNavDocOverlay").style.left = navWidth;    
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("sideNav").style.left = "-250px";
-  document.getElementById("sideNavDocOverlay").style.zIndex = "-1000";
-  document.getElementById("sideNavDocOverlay").style.backgroundColor = "rgba(0,0,0,0.0)";
-  document.getElementById("sideNavDocOverlay").style.left = "0px"
+    let navOffset = getNavWidth()[1];
+
+    console.log(navOffset);
+
+    document.getElementById("sideNav").style.left = navOffset;
+    document.getElementById("sideNavDocOverlay").style.zIndex = "-1000";
+    document.getElementById("sideNavDocOverlay").style.backgroundColor = "rgba(0,0,0,0.0)";
+    document.getElementById("sideNavDocOverlay").style.left = "0px"
+
 }
