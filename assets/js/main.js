135220c8f81e8eabed6d1882159404d5d1ceac15
BWeeks101 <bweeks101@gmail.com>
2020-11-15 22:54:36 +0000

    Added initial styling and functions to dynamically adjust page content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,18 @@
 $(document).ready(mainOnLoad);
 
 function mainOnLoad() {
-    menuWidth = calcMenuWidth();
-    document.getElementById("menuBlockRow").style.width = menuWidth;
-    let footerTop = getElementPos("footer").top;
-    let mainBlockBottom = getElementPos("mainBlock").bottom;    
-    console.log("footerTop: " + footerTop + "  mainBlockBottom: " + mainBlockBottom);
-    console.log(document.getElementById("menuBlockRow").style.Height = footerTop - mainBlockBottom + "px");
-    document.getElementById("menuBlockRow").style.height = footerTop - mainBlockBottom + "px";
+    let menuWidth = calcMenuWidth();
+    let menuHeight = calcMenuHeight();
+    document.getElementById("menuBlock").style.width = menuWidth;
+    document.getElementById("menuBlock").style.height = menuHeight;
+    
 
     let test = getElementPos("header")
     test = getElementPos("mainBlock")
-    test = getElementPos("menuBlockRow")
+    test = getElementPos("menuBlock")
     test = getElementPos("footer")
 }
+
 
 /* Get position and size of element */
 function getElementPos(elementId) {
@@ -22,15 +21,27 @@ function getElementPos(elementId) {
     elementPos.right = elementPos.right + window.pageXOffset;
     elementPos.bottom = elementPos.bottom + window.pageYOffset;
     elementPos.left = elementPos.left + window.pageXOffset;
+    console.log(window.pageYOffset);
     console.log(elementPos);
     return elementPos;
+}
+
+/* Calculate desired height of menuBlock */
+function calcMenuHeight() {
+    let cHeight = window.innerHeight;
+    let headerHeight = 66;
+    let mainBlockImgHeight = getElementPos("mainBlockContainer").height;
+    let footerHeight = 50;
+
+    let menuHeight = cHeight - (headerHeight + mainBlockImgHeight + footerHeight);
+
+    return `${menuHeight}px`;
 }
 
 /* Calculate desired width of menuBlock */
 function calcMenuWidth() {
     let cWidth = window.innerWidth;
     let percentage = 90;
-
     let menuWidth = (cWidth/100)*percentage;
 
     if (menuWidth < 324) {
