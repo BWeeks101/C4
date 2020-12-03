d7eb1bbaa80d37ac765ab289645817dbcf536005
BWeeks101 <bweeks101@gmail.com>
2020-12-03 17:55:39 +0000

    Removed unnecessary debug line from game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,8 +13,6 @@ let gameState = [
 /* Game Board */
 let gBoardHeaders = ["","","","","","",""];
 let gBoardDG = new DataGrid(gBoardHeaders, gameState);
-
-console.log(gBoardDG)
 /* end Game Board */
 
 function gameClicked(object) {
