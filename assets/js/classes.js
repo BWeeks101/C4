b1b011ea3b0c9cc919333a43f1422aee010d8713
BWeeks101 <bweeks101@gmail.com>
2020-12-10 10:13:50 +0000

    Removed unnecessary classes from classes.js

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -1,42 +1,4 @@
 /* Global settings objects*/
-class navBarDefaultColors {
-    constructor() {        
-        this.darkToggler = "rgba(250,250,250,.4)";
-        this.togglerHoverBorder = "rgba(236,76,76,.4)";
-        this.togglerBackground = "#EC4C4C";
-        this.togglerHoverIcon = "#EC4C4C";
-        this.togglerIconMaskURL = "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%28250, 250, 250, 1%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e";
-        this.togglerIconBackground = "#fafafa";
-    }
-}
-
-class sideNavDefaultColors {
-    constructor() {
-        this.docOverlayBackground = "rgba(0, 0, 0, 0)";
-        this.background = "rgba(45,45,45,.95)";
-        this.divider = "#fafafa";
-        this.link = "#fafafa";
-        this.linkHover = "#EC4C4C";
-    }
-}
-
-class dataGridDefaultColors {
-    constructor() {
-        this.colSelected = "#EC4C4C";
-        this.rowSelected = "#EC4C4C";
-        this.border = "#fafafa";
-    }
-}
-
-class gameDefaultColors {
-    constructor() {
-        this.border = "#fafafa";
-        this.feedbackBackground = "#2D2D2D";
-        this.feedbackText = "#fafafa";
-        this.pulseBackground = "#fafafa";
-    }
-}
-
 class playerSettings {
     constructor() {
         this.name;
@@ -62,7 +24,6 @@ class gameSettings {
             [undefined, undefined, undefined, undefined, undefined, undefined]
         ];
         this.gBoardDG = new DataGrid(["","","","","","",""], this.boardState);
-        this.colors = new gameDefaultColors();
         this.p1 = new playerSettings();        
         this.p1.name = "Player 1";
         this.p1.tokenColor = "#EC4C4C";
@@ -74,25 +35,8 @@ class gameSettings {
     }
 }
 
-class appDefaultColors {
-    constructor() {
-        this.text = "#fafafa";
-        this.border = "#fafafa";
-        this.background = "#262626";
-        this.altBackground = "#2D2D2D";
-        this.headerDivider = "#CCCCCC";
-        this.footerDivider = "#CCCCCC";
-        this.socialMediaLink = "#fafafa";
-        this.socialMediaLinkHover = "#EC4C4C";
-        this.navBar = new navBarDefaultColors();
-        this.sideNav = new sideNavDefaultColors();
-        this.dataGrid = new dataGridDefaultColors();
-    }
-}
-
 class appGlobals {
     constructor() {
-        this.colors = new appDefaultColors();
         this.game = new gameSettings();
     }
 }
