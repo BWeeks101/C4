71b7d5c3c3e3d50d44c3943c51dd601dc25dab9e
BWeeks101 <bweeks101@gmail.com>
2020-12-18 17:05:15 +0000

    Added comments to feedbackStartDelay()

--- assets/js/game.js
+++ assets/js/game.js
@@ -37,22 +37,26 @@ function feedbackWinner(result) {
     elementDisplay("show", "feedbackContainer"); //Show the feedback container
 }
 
+/* Before a game starts, use the feedback message element do display a 5 second countdown timer, then start the game */
 function feedbackStartDelay() {
+    /* Remove any color styling from the feedback message, display the countdown text, hide the feedback buttons and show the feedback container */
     document.getElementById("feedbackMessage").style.removeProperty("color");
     document.getElementById("feedbackMessage").innerHTML = `<h2>Game Start In:</h2><h2 id="startDelay">5</h2>`;
     elementDisplay("hide", "feedbackControlRow");
     elementDisplay("show", "feedbackContainer");
+
+    /* Execute once a second, reducing the countdown text each time until we hit 0, then start the game */
     startDelay = setInterval(function() {
-        timerVal = parseInt(document.getElementById("startDelay").innerHTML);
+        timerVal = parseInt(document.getElementById("startDelay").innerHTML); //Get the current integer
         if (timerVal > 0) {
-            document.getElementById("startDelay").innerHTML = `${timerVal-1}`;
+            document.getElementById("startDelay").innerHTML = `${timerVal-1}`; //If we're not at 0, then reduce the timer by 1
         } else {
-            stopStartDelay();
-            elementDisplay("hide", "feedbackContainer");
-            elementDisplay("show", "feedbackControlRow");
-            document.getElementById("feedbackMessage").innerHTML = "";
-            startTurnTimer();
-            showGameSideNavMenu();
+            stopStartDelay(); //We're at 0, so stop the timer
+            elementDisplay("hide", "feedbackContainer"); //Hide the feedback container
+            elementDisplay("show", "feedbackControlRow"); //Show the feedback button (they will not be displayed until the feedback container is shown)
+            document.getElementById("feedbackMessage").innerHTML = ""; //Clear the feedback message innerHTML
+            startTurnTimer(); //Start the turn timer (and therefore the game)
+            showGameSideNavMenu(); //Show the relevant links on the sideNav
         }
     }, 1000);    
 }
