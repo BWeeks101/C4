5f3a397c37ba90e62f0a079681af9a58c8171d99
BWeeks101 <bweeks101@gmail.com>
2021-01-13 11:41:47 +0000

    Parsed main.js with JSHint, and JSLint.  Tidied whitespace and improved variable declarations.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ let c4 = new appGlobals();
 $(document).ready(mainOnLoad);
 
 /* on Load */
-function mainOnLoad() {    
+function mainOnLoad() {
     c4.uiState = "default"; //Set the default state
     loadColorMode(); //Apply the appropriate theme (light or dark(default))
     sideNavOnLoad(); //Initialise the sideNav
@@ -52,12 +52,12 @@ function switchColorMode(ihtml) {
 }
 
 /* Load the appropriate theme per localStorage colorMode kv pair */
-function loadColorMode() {    
-    if (localStorage.getItem("colorMode") == "light") {        
+function loadColorMode() {
+    if (localStorage.getItem("colorMode") == "light") {
         switchColorMode("Light Mode");
-    } else {        
+    } else {
         switchColorMode("Dark Mode");
-    }    
+    }
 }
 
 function logoResize() {
@@ -67,23 +67,23 @@ function logoResize() {
 
     if (document.getElementById("logoGrid").classList.contains("d-none") == false) {
         if (c4.logo.animState == false) {
-            gridCounts = dataGridDisplayGetCounts("logoGrid"); //Get the number of rows and columns for the logoGrid element
+            let gridCounts = dataGridDisplayGetCounts("logoGrid"); //Get the number of rows and columns for the logoGrid element
             if (gridCounts == false) { //If false, then we could not return the values, so return false
-                console.log(`function logoResize failed.  Cascade failure originating with dataGridDisplayGetCounts("logoGrid").`)
+                console.log(`function logoResize failed.  Cascade failure originating with dataGridDisplayGetCounts("logoGrid").`);
                 return false;
             }
-            
+
             logoFontResize();
-            
+
             setlogoGridContentRowMarginTop();
-            
+
             let colCenter = getLogoGridColCenter(); //Get the center point of each header and column cell
             if (typeof colCenter != "object") { //If colCenter is not an object then we could not get the center points, so return false
                 console.log(`function logoResize failed.  Unable to get center points.  Cascade failure originating with getLogoGridColCenter()`);
                 return false;
             }
 
-            for (i = 0; i < gridCounts[0]; i++) {
+            for (let i = 0; i < gridCounts[0]; i++) {
                 if (document.getElementById(`logoGridHcol-${i}`).firstElementChild.style.left != "") {
                     colCenter.headerCenter[i].absoluteX = colCenter.headerCenter[i].absoluteX - parseFloat(document.getElementById(`logoGridHcol-${i}`).firstElementChild.style.left);
                 }
@@ -91,44 +91,44 @@ function logoResize() {
 
                 if (document.getElementById(`logoGridHcol-${i}`).firstElementChild.style.top != "") {
                     colCenter.headerCenter[i].absoluteY = colCenter.headerCenter[i].absoluteY - parseFloat(document.getElementById(`logoGridHcol-${i}`).firstElementChild.style.top);
-                }                            
+                }
                 document.getElementById(`logoGridHcol-${i}`).firstElementChild.style.top = `${(colCenter.contentCenter[i].absoluteY - colCenter.headerCenter[i].absoluteY)}px`;
-            }            
+            }
         }
     }
 }
 
 /* Font Resize */
-function logoFontResize() {    
-    let minFontSizePx = parseFloat(getComputedStyle(document.getElementById("logoGridHcol-0").firstElementChild).getPropertyValue(`font-size`).trim());; //Min Supported Logo Font Size in px    
+function logoFontResize() {
+    let minFontSizePx = parseFloat(getComputedStyle(document.getElementById("logoGridHcol-0").firstElementChild).getPropertyValue(`font-size`).trim()); //Min Supported Logo Font Size in px
     let minLogoContentSize = parseFloat(getComputedStyle(document.getElementById("logoGridCol0RowId0").firstElementChild.lastElementChild).getPropertyValue(`min-width`).trim()); //Min Logo Content Size in px
     let maxLogoContentSize = parseFloat(getComputedStyle(document.getElementById("logoGridCol0RowId0").firstElementChild.lastElementChild).getPropertyValue(`max-width`).trim()); //Max Logo Content Size in px
-    let fontLogoScale = (minFontSizePx / minLogoContentSize) * 100 //Percentage difference betwen font and logo content    
-    let maxFontSizePx = (maxLogoContentSize / 100) * fontLogoScale; //Max Logo Font Size in px    
+    let fontLogoScale = (minFontSizePx / minLogoContentSize) * 100; //Percentage difference betwen font and logo content
+    let maxFontSizePx = (maxLogoContentSize / 100) * fontLogoScale; //Max Logo Font Size in px
 
     let gridCounts = dataGridDisplayGetCounts("logoGrid"); //Get the number of rows and columns for the logoGrid element
     if (gridCounts == false) { //If false, then we could not return the values, so return false
-        console.log(`function logoResize failed.  Cascade failure originating with dataGridDisplayGetCounts("logoGrid").`)
+        console.log(`function logoResize failed.  Cascade failure originating with dataGridDisplayGetCounts("logoGrid").`);
         return false;
     }
 
     let contentWidth = getElementPos(document.getElementById(`logoGridCol0RowId0`).firstElementChild.lastElementChild).width;
     let checkWidth;
-    for (i = 1; i < gridCounts[0]; i++) {
+    for (let i = 1; i < gridCounts[0]; i++) {
         checkWidth = getElementPos(document.getElementById(`logoGridCol${i}RowId0`).firstElementChild.lastElementChild).width;
         if (checkWidth < contentWidth) {
             contentWidth = checkWidth; //Use the smallest contentWidth to ensure text fits all
         }
     }
 
-    fontSize = (contentWidth / 100) * fontLogoScale; //Calculate font size based on current content col width, and the fontLogoScale value
+    let fontSize = (contentWidth / 100) * fontLogoScale; //Calculate font size based on current content col width, and the fontLogoScale value
     if (fontSize > maxFontSizePx) {
         fontSize = maxFontSizePx; //Do not exceed max font size in px
     } else if (fontSize < minFontSizePx) {
         fontSize = minFontSizePx; //Do not exceed min font size in px
-    }    
+    }
 
-    for (i = 0; i < gridCounts[0]; i++) {
+    for (let i = 0; i < gridCounts[0]; i++) {
         document.getElementById(`logoGridHcol-${i}`).firstElementChild.firstElementChild.style.fontSize = `${fontSize}px`;
     }
 }
@@ -139,38 +139,38 @@ function mainBlockResize() {
     /* Remove any existing height or padding-bottom style properties from the main block container */
     document.getElementById("mainBlockContainer").style.removeProperty("height");
     document.getElementById("mainBlockContainer").style.removeProperty("padding-bottom");
-    
+
     /* If the menu block container is not visible, then calculate and apply the correct height and padding-bottom styles to the main block */
     if (document.getElementById("menuBlockContainer").classList.contains("d-none") == true) {
-        mainBlockContainerHeight = calcBlockHeight();
+        let mainBlockContainerHeight = calcBlockHeight();
         document.getElementById("mainBlockContainer").style.height = `${mainBlockContainerHeight}px`;
-        mainBlockContainerPadBottom = getElementPropertyVal("mainBlockContainer", "padding-top", "int");
+        let mainBlockContainerPadBottom = getElementPropertyVal("mainBlockContainer", "padding-top", "int");
         document.getElementById("mainBlockContainer").style.paddingBottom = `${mainBlockContainerPadBottom}px`;
 
         /* If the rules container is visible, then calculate and apply height and width properties for it and it's children to ensure we do not scroll the page */
         if (document.getElementById("rulesContainer").classList.contains("d-none") == false) {
 
             /* Remove any existing height style properties from the rules container and any appropriate children */
-            document.getElementById("rulesContainer").style.removeProperty("height")
-            document.getElementById("rulesMainContent").style.removeProperty("height")
+            document.getElementById("rulesContainer").style.removeProperty("height");
+            document.getElementById("rulesMainContent").style.removeProperty("height");
 
             /* Get the total padding value of the mainBlock (parent of the rules container) */
-            mainBlockContainerPadTop = getElementPropertyVal("mainBlockContainer", "padding-top", "int");
+            let mainBlockContainerPadTop = getElementPropertyVal("mainBlockContainer", "padding-top", "int");
             mainBlockContainerPadBottom = getElementPropertyVal("mainBlockContainer", "padding-bottom", "int");
-            mainBlockContainerPadTotal = mainBlockContainerPadTop + mainBlockContainerPadBottom;
+            let mainBlockContainerPadTotal = mainBlockContainerPadTop + mainBlockContainerPadBottom;
 
             /* Set the rules container height to equal the height of the parent - padding */
-            rulesContainerHeight = mainBlockContainerHeight - mainBlockContainerPadTotal;
+            let rulesContainerHeight = mainBlockContainerHeight - mainBlockContainerPadTotal;
             document.getElementById("rulesContainer").style.height = `${rulesContainerHeight}px`;
 
             /* Set the height of the rules main container element */
-            rulesControlButtonsHeight = getElementPos(document.getElementById("rulesContainer").lastElementChild).height;
-            rulesMainContentHeight = rulesContainerHeight - rulesControlButtonsHeight;
+            let rulesControlButtonsHeight = getElementPos(document.getElementById("rulesContainer").lastElementChild).height;
+            let rulesMainContentHeight = rulesContainerHeight - rulesControlButtonsHeight;
             document.getElementById("rulesMainContent").style.height = `${rulesMainContentHeight}px`;
-            
+
             return;
         }
-        
+
         /* If the game board container is visible, then calculate and apply height and width properties for it and it's children to ensure we do not scroll the page */
         if (document.getElementById("gameBoardContainer").classList.contains("d-none") == false) {
             /* Remove any existing height, max-height, or max-width style properties from the game board container and appropriate children */
@@ -182,52 +182,52 @@ function mainBlockResize() {
             document.getElementById("gBoard").style.removeProperty("height");
 
             /* Get the total padding value of the mainBlock (parent of the game board container) */
-            mainBlockContainerPadTop = getElementPropertyVal("mainBlockContainer", "padding-top", "int");
+            let mainBlockContainerPadTop = getElementPropertyVal("mainBlockContainer", "padding-top", "int");
             mainBlockContainerPadBottom = getElementPropertyVal("mainBlockContainer", "padding-bottom", "int");
-            mainBlockContainerPadTotal = mainBlockContainerPadTop + mainBlockContainerPadBottom;
+            let mainBlockContainerPadTotal = mainBlockContainerPadTop + mainBlockContainerPadBottom;
 
             /* Set the game board container height to equal the height of the parent - padding */
-            gameBoardContainerHeight = mainBlockContainerHeight - mainBlockContainerPadTotal;
+            let gameBoardContainerHeight = mainBlockContainerHeight - mainBlockContainerPadTotal;
             document.getElementById("gameBoardContainer").style.height = `${gameBoardContainerHeight}px`;
 
             /* Set the height of the game container element */
-            playerInfoContainerHeight = getElementPos("playerInfoContainer").height; //Get the height of the player info container
-            gameContainerHeight = gameBoardContainerHeight - playerInfoContainerHeight; //The game container height = game board container height - player info container height
+            let playerInfoContainerHeight = getElementPos("playerInfoContainer").height; //Get the height of the player info container
+            let gameContainerHeight = gameBoardContainerHeight - playerInfoContainerHeight; //The game container height = game board container height - player info container height
             document.getElementById("gameContainer").style.height = `${gameContainerHeight}px`;
 
             /* Set the height of the feedback container, message and control rows */
-            feedbackMessageHeight = getElementPos("feedbackMessage").height; //Get the height of the feedback message
-            feedbackControlRowHeight = getElementPos("feedbackControlRow").height; //Get the height of the control row
-            feedbackContainerHeight = feedbackMessageHeight + feedbackControlRowHeight; //the container height = message height + control row height, with a minimum of 87px.
+            let feedbackMessageHeight = getElementPos("feedbackMessage").height; //Get the height of the feedback message
+            let feedbackControlRowHeight = getElementPos("feedbackControlRow").height; //Get the height of the control row
+            let feedbackContainerHeight = feedbackMessageHeight + feedbackControlRowHeight; //the container height = message height + control row height, with a minimum of 87px.
             if (feedbackContainerHeight < 87) {
                 feedbackContainerHeight = 87;
             }
             document.getElementById("feedbackContainer").style.height = `${feedbackContainerHeight}px`;
 
             /* Set the max height of the gboard content container */
-            gBoardContentContainerMaxHeight = gameContainerHeight - feedbackContainerHeight; //gboard content container max height = game container height - feedback container height
+            let gBoardContentContainerMaxHeight = gameContainerHeight - feedbackContainerHeight; //gboard content container max height = game container height - feedback container height
             document.getElementById("gBoardContentContainer").style.maxHeight = `${gBoardContentContainerMaxHeight}px`;
-            
+
             /* Set the max width of the game board container */
-            gameBoardContainerMaxWidth = (gBoardContentContainerMaxHeight / 85.8) * 100;  //game board container max width =  116.55% of the game board container max height
+            let gameBoardContainerMaxWidth = (gBoardContentContainerMaxHeight / 85.8) * 100;  //game board container max width =  116.55% of the game board container max height
             document.getElementById("gameBoardContainer").style.maxWidth = `${gameBoardContainerMaxWidth}px`;
-            
+
             /* Set the height of the gboard to equal the height of the gboard content container */
-            gBoardContentContainerHeight = getElementPos("gBoardContentContainer").height;
-            gBoardHeight = gBoardContentContainerHeight;
+            let gBoardContentContainerHeight = getElementPos("gBoardContentContainer").height;
+            let gBoardHeight = gBoardContentContainerHeight;
             document.getElementById("gBoard").style.height = `${gBoardHeight}px`;
 
             return;
         }
-    }    
+    }
 }
 
 /* Resize the menuBlock */
 /* This block contains the initial options pane, player settings pane, and turn time limit pane */
 function menuBlockResize() {
     /* calculate the menu block height and set it */
-    menuBlockHeight = calcBlockHeight();
-    document.getElementById("menuBlock").style.height = `${menuBlockHeight}px`;    
+    let menuBlockHeight = calcBlockHeight();
+    document.getElementById("menuBlock").style.height = `${menuBlockHeight}px`;
 }
 
 /* Get Computed Property of element */
@@ -278,10 +278,10 @@ function getElementPropertyVal(elementId, propertyName, resFormat) {
 function getElementPos(element) {
     let elementPos;
     if (typeof element == "string") {
-        elementPos = document.getElementById(element).getBoundingClientRect();    
+        elementPos = document.getElementById(element).getBoundingClientRect();
     } else if (typeof element == "object") {
         elementPos = element.getBoundingClientRect();
-    }    
+    }
     elementPos.top = elementPos.top + window.pageYOffset;
     elementPos.right = elementPos.right + window.pageXOffset;
     elementPos.bottom = elementPos.bottom + window.pageYOffset;
@@ -304,7 +304,7 @@ function calcBlockHeight() {
     let wHeight = window.innerHeight;
 
     /* Min Supported Browser Height */
-    let minWHeight = parseFloat(getComputedStyle(document.getElementsByTagName("body")[0]).getPropertyValue(`min-height`).trim())
+    let minWHeight = parseFloat(getComputedStyle(document.getElementsByTagName("body")[0]).getPropertyValue(`min-height`).trim());
     if (wHeight < minWHeight) {
         wHeight = minWHeight;
     }
@@ -321,13 +321,13 @@ function calcBlockHeight() {
 
     /* mainBlockContainer Height */
     let mainBlockContainerHeight;
-    if (mainBlockContainerHidden == false) {        
+    if (mainBlockContainerHidden == false) {
         mainBlockContainerHeight = getElementPos("mainBlockContainer").height;
     }
 
     /* Is menuBlockContainer Hidden */
     let menuBlockContainerHidden = document.getElementById("menuBlockContainer").classList.contains("d-none");
-    
+
     /* Calculated Block Height and Full Content Height */
     let blockHeight;
     let contentHeight;
@@ -341,7 +341,7 @@ function calcBlockHeight() {
     } else if (mainBlockContainerHidden == false && menuBlockContainerHidden == true) {
         blockHeight = mainHeight;
         contentHeight = headerHeight + blockHeight + footerHeight;
-    }    
+    }
 
     /* If Calculated Full Content Height is Greater than Browser Viewport */
     /* Reduce Block Height by the difference to prevent vertical scroll, respecting minimum height */
@@ -366,7 +366,7 @@ function elementDisplay(action, elementID) {
             break;
         case "hide":
             document.getElementById(elementID).classList.add("d-none"); //do not display the element
-            break;            
+            break;
     }
 }
 
@@ -394,7 +394,7 @@ function hideAll(elementId) {
 
     /* Iterate through the collection, hiding all elements */
     for (i; i < elementCollection.length; i++) {
-        elementDisplay("hide", elementCollection[i].id);        
+        elementDisplay("hide", elementCollection[i].id);
     }
 
     /* If elementId = mainBlockContainer then also hide the menuBlockContainer element and children */
