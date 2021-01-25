fd4b4e0ebc02153af850b89dffe104cb7c71f10b
BWeeks101 <bweeks101@gmail.com>
2021-01-25 20:00:47 +0000

    Adjusted mainBlockResize to prevent window scroll when the game board is centered

--- assets/js/main.js
+++ assets/js/main.js
@@ -372,8 +372,11 @@ function mainBlockResize() {
             playerInfoFontResize();
             playerInfoContainerHeight = getElementPos("playerInfoContainer").height; //Get the height of the player info container
 
-            let playerInfoContainerMarginTop = (gameBoardContainerHeight - (playerInfoContainerHeight + feedbackContainerHeight + gBoardHeight)) / 2;
+            let playerInfoContainerMarginTop = (gameBoardContainerHeight - (playerInfoContainerHeight + feedbackContainerHeight + gBoardHeight)) / 2;            
             document.getElementById("playerInfoContainer").style.marginTop = `${playerInfoContainerMarginTop}px`;
+
+            gameContainerHeight = gBoardHeight + feedbackContainerHeight; //Reduce the height of the game container element to fit the height of the game board and feedback container elements
+            document.getElementById("gameContainer").style.height = `${gameContainerHeight}px`;
 
             return;
         }
